f0119e97501833f01ee40d738f82b028f1329be1
Rafael Scorfi Gatto <rscorfi@gmail.com>
2025-06-02 19:36:26 -0300

    Add ex08 aula 06 JS

---
+++ exercícios txt/exercicio_08.js
@@ -0,0 +1,7 @@
+// 8. Verifique se dois números são iguais e se ambos são maiores que 10.
+
+let a = Number(prompt("Informe um número: "))
+let b = Number(prompt("Informe outro número: "))
+
+let resultado = (a == b) && (a > 10) && (b > 10)
+console.log(resultado)
